45b96f2de3a0ae73c1a725621af5bf92b267d5d9
root <1095331784@qq.com>
2020-11-15 10:43:52 -0500

    hhd

---
+++ src/views/lk_blog/tag/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="内容" prop="content">
+            <el-input v-model="form.content" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="color" prop="color">
+            <el-input v-model="form.color" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="tagId" label="ID" />
+        <el-table-column prop="content" label="内容" />
+        <el-table-column prop="color" label="color" />
+        <el-table-column prop="createTime" label="创建日期">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.createTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="updateTime" label="更新时间">
+          <template slot-scope="scope">
+            <span>{{ parseTime(scope.row.updateTime) }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column v-permission="['admin','tag:edit','tag:del']" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudTag from '@/api/tag'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = { tagId: null, content: null, color: null, createTime: null, updateTime: null }
+export default {
+  name: 'Tag',
+  components: { pagination, crudOperation, rrOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  cruds() {
+    return CRUD({ title: 'lkblog_标签', url: 'api/tag', idField: 'tagId', sort: 'tagId,desc', crudMethod: { ...crudTag }})
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'tag:add'],
+        edit: ['admin', 'tag:edit'],
+        del: ['admin', 'tag:del']
+      },
+      rules: {
+        content: [
+          { required: true, message: '内容不能为空', trigger: 'blur' }
+        ],
+        color: [
+          { required: true, message: 'color不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
