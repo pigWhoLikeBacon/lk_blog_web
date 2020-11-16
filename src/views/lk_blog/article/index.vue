7afd9819a749a94a2497f437a0576dacf56833b9
root <1095331784@qq.com>
2020-11-16 02:04:57 -0500

    hhd

--- src/views/lk_blog/article/index.vue
+++ src/views/lk_blog/article/index.vue
@@ -7,11 +7,20 @@
       <!--表单组件-->
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
         <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-          <el-form-item label="内容" prop="content">
-            <el-input v-model="form.content" style="width: 370px;" />
+          <el-form-item label="文章封面" prop="cover">
+            <el-input v-model="form.cover" style="width: 370px;" />
           </el-form-item>
-          <el-form-item label="color" prop="color">
-            <el-input v-model="form.color" style="width: 370px;" />
+          <el-form-item label="简介" prop="introduce">
+            <el-input v-model="form.introduce" :rows="3" type="textarea" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="题目" prop="title">
+            <el-input v-model="form.title" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="内容" prop="content">
+            <el-input v-model="form.content" :rows="3" type="textarea" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="是否展示" prop="isShow">
+            <el-radio v-model="form.isShow" v-for="item in dict.article_show" :key="item.id" :label="item.value">{{ item.label }}</el-radio>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -22,9 +31,17 @@
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="selection" width="55" />
-        <el-table-column prop="tagId" label="ID" />
+        <el-table-column prop="articleId" label="ID" />
+        <el-table-column prop="cover" label="文章封面" />
+        <el-table-column prop="introduce" label="简介" />
+        <el-table-column prop="title" label="题目" />
         <el-table-column prop="content" label="内容" />
-        <el-table-column prop="color" label="color" />
+        <el-table-column prop="views" label="浏览量" />
+        <el-table-column prop="isShow" label="是否展示">
+          <template slot-scope="scope">
+            {{ dict.label.article_show[scope.row.isShow] }}
+          </template>
+        </el-table-column>
         <el-table-column prop="createTime" label="创建日期">
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.createTime) }}</span>
@@ -35,7 +52,7 @@
             <span>{{ parseTime(scope.row.updateTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column v-permission="['admin','tag:edit','tag:del']" label="操作" width="150px" align="center">
+        <el-table-column v-permission="['admin','article:edit','article:del']" label="操作" width="150px" align="center">
           <template slot-scope="scope">
             <udOperation
               :data="scope.row"
@@ -51,34 +68,44 @@
 </template>
 
 <script>
-import crudTag from '@/api/tag'
+import crudArticle from '@/api/article'
 import CRUD, { presenter, header, form, crud } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
 
-const defaultForm = { tagId: null, content: null, color: null, createTime: null, updateTime: null }
+const defaultForm = { articleId: null, cover: null, introduce: null, title: null, content: null, views: null, isShow: null, createTime: null, updateTime: null }
 export default {
-  name: 'Tag',
+  name: 'Article',
   components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(), header(), form(defaultForm), crud()],
+  dicts: ['article_show'],
   cruds() {
-    return CRUD({ title: 'lkblog_文章', url: 'api/tag', idField: 'tagId', sort: 'tagId,desc', crudMethod: { ...crudTag }})
+    return CRUD({ title: 'lkblog_文章', url: 'api/article', idField: 'articleId', sort: 'articleId,desc', crudMethod: { ...crudArticle }})
   },
   data() {
     return {
       permission: {
-        add: ['admin', 'tag:add'],
-        edit: ['admin', 'tag:edit'],
-        del: ['admin', 'tag:del']
+        add: ['admin', 'article:add'],
+        edit: ['admin', 'article:edit'],
+        del: ['admin', 'article:del']
       },
       rules: {
+        cover: [
+          { required: true, message: '文章封面不能为空', trigger: 'blur' }
+        ],
+        introduce: [
+          { required: true, message: '简介不能为空', trigger: 'blur' }
+        ],
+        title: [
+          { required: true, message: '题目不能为空', trigger: 'blur' }
+        ],
         content: [
           { required: true, message: '内容不能为空', trigger: 'blur' }
         ],
-        color: [
-          { required: true, message: 'color不能为空', trigger: 'blur' }
+        isShow: [
+          { required: true, message: '是否展示不能为空', trigger: 'blur' }
         ]
       }    }
   },
